659711244aa4dc98422c9d11af61298caf74d315
Evandro Harrison Hoffmann <ehh85@yahoo.com.br>
2016-11-23 16:53:47 +0800

    Fixes basic values for animations.

--- MVUIHacks/Classes/Extensions/UIView+Animation.swift
+++ MVUIHacks/Classes/Extensions/UIView+Animation.swift
@@ -10,7 +10,7 @@ import UIKit
 
 extension UIView{
     
-    public func animateShowPopingUp(duration: Double = 0.2, scaleIn: CGFloat = 0.1, scaleOut: CGFloat = 1.05, alphaIn: CGFloat = 0, _ completion:(() -> Void)?){
+    public func animateShowPopingUp(duration: Double = 0.2, scaleIn: CGFloat = 0.1, scaleOut: CGFloat = 1.05, alphaIn: CGFloat = 0, _ completion:(() -> Void)? = nil){
         isHidden = false
         alpha = alphaIn
         transform = CGAffineTransform(scaleX: scaleIn, y: scaleIn)
@@ -31,7 +31,7 @@ extension UIView{
         })
     }
     
-    public func animateFadeInUp(duration: Double = 0.3, alphaIn: CGFloat = 0, _ completion:(() -> Void)?){
+    public func animateFadeInUp(duration: Double = 0.3, alphaIn: CGFloat = 0, _ completion:(() -> Void)? = nil){
         self.alpha = alphaIn
         
         let center = self.center
@@ -47,7 +47,7 @@ extension UIView{
         })
     }
     
-    public func animateFadeIn(duration: Double = 0.3, alphaIn: CGFloat = 0, _ completion:(() -> Void)?){
+    public func animateFadeIn(duration: Double = 0.3, alphaIn: CGFloat = 0, _ completion:(() -> Void)? = nil){
         self.alpha = alphaIn
         
         UIView.animate(withDuration: duration, animations: {
@@ -59,7 +59,7 @@ extension UIView{
         })
     }
     
-    public func animateFadeOut(duration: Double = 0.3, _ completion:(() -> Void)?){
+    public func animateFadeOut(duration: Double = 0.3, _ completion:(() -> Void)? = nil){
         UIView.animate(withDuration: duration, animations: {
             self.alpha = 0
         }, completion: { (didComplete) in
@@ -69,7 +69,7 @@ extension UIView{
         })
     }
     
-    public func animateFadeAway(_ completion:(() -> Void)?){
+    public func animateFadeAway(_ completion:(() -> Void)? = nil){
         UIView.animate(withDuration: 0.2, animations: {
             self.transform = CGAffineTransform(scaleX: 1.1, y: 1.1)
         }, completion: { (didComplete) in
@@ -87,7 +87,7 @@ extension UIView{
         })
     }
     
-    public func animateTouchDown(duration: Double = 0.2, scaleIn: CGFloat = 0.1, alphaIn: CGFloat = 0, halfWay:(() -> Void)? = nil, _ completed:(() -> Void)? = nil){
+    public func animateTouchDown(duration: Double = 0.2, scaleIn: CGFloat = 0.9, alphaIn: CGFloat = 0, halfWay:(() -> Void)? = nil, _ completed:(() -> Void)? = nil){
         UIView.animate(withDuration: duration*0.5, animations: {
             self.alpha = alphaIn
             self.transform = CGAffineTransform(scaleX: scaleIn, y: scaleIn)
