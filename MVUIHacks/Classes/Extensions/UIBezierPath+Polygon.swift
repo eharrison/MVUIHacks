eed881c2ba11b2c5b9bfe9a2f76b70d6f9e23c8c
Antônio da Silva <antoniocarlos.dev@gmail.com>
2017-06-28 18:48:23 +0800

    Minor changes

--- MVUIHacks/Classes/Extensions/UIBezierPath+Polygon.swift
+++ MVUIHacks/Classes/Extensions/UIBezierPath+Polygon.swift
@@ -13,7 +13,7 @@ extension UIBezierPath {
     public convenience init(roundedPolygonPathWithRect rect: CGRect, lineWidth: CGFloat, sides: NSInteger, cornerRadius: CGFloat) {
         self.init()
         
-        let theta = CGFloat(2.0 * M_PI) / CGFloat(sides)                            // how much to turn at every corner
+        let theta = CGFloat(2.0 * Double.pi) / CGFloat(sides)                            // how much to turn at every corner
         let offset = CGFloat(cornerRadius) * CGFloat(tan(theta / 2.0))              // offset from which to start rounding corners
         let squareWidth = min(rect.size.width, rect.size.height)          // width of the square
         
@@ -28,7 +28,7 @@ extension UIBezierPath {
         // start drawing at `point` in lower right corner
         
         var point = CGPoint(x: squareWidth / 2.0 + sideLength / 2.0 - offset, y: squareWidth - (squareWidth - length) / 2.0)
-        var angle = CGFloat(M_PI)
+        var angle = CGFloat(Double.pi)
         move(to: point)
         
         // draw the sides and rounded corners of the polygon
@@ -37,8 +37,8 @@ extension UIBezierPath {
             point = CGPoint(x: point.x + CGFloat(sideLength - offset * 2.0) * CGFloat(cos(angle)), y: point.y + CGFloat(sideLength - offset * 2.0) * CGFloat(sin(angle)))
             addLine(to: point)
             
-            let center = CGPoint(x: point.x + cornerRadius * CGFloat(cos(angle + CGFloat(M_PI_2))), y: point.y + cornerRadius * CGFloat(sin(angle + CGFloat(M_PI_2))))
-            addArc(withCenter: center, radius:CGFloat(cornerRadius), startAngle:angle - CGFloat(M_PI_2), endAngle:angle + theta - CGFloat(M_PI_2), clockwise:true)
+            let center = CGPoint(x: point.x + cornerRadius * CGFloat(cos(angle + CGFloat(Double.pi/2))), y: point.y + cornerRadius * CGFloat(sin(angle + CGFloat(Double.pi/2))))
+            addArc(withCenter: center, radius:CGFloat(cornerRadius), startAngle:angle - CGFloat(Double.pi/2), endAngle:angle + theta - CGFloat(Double.pi/2), clockwise:true)
             
             point = currentPoint // we don't have to calculate where the arc ended ... UIBezierPath did that for us
             angle += theta
